8662b3fb4d354b0de63e3619de7c8665636429fd
Dave Powerman <davepowerman@gmail.com>
2014-05-10 19:28:28 -0400

    edit deploy_settings.py

--- davepowerman/deploy_settings.py
+++ davepowerman/deploy_settings.py
@@ -6,12 +6,12 @@ DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-#DATABASES['default'] =  dj_database_url.config()
-DATABASES = {
-  "default": {
-     "ENGINE": "django.db.backends.postgresql_psycopg2",
-  }
-}
+DATABASES['default'] =  dj_database_url.config()
+#DATABASES = {
+#  "default": {
+#     "ENGINE": "django.db.backends.postgresql_psycopg2",
+#  }
+#}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
